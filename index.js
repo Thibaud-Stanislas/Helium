565a94d3a5f9abfa186058270377282d4507f825
Thibaud-Stanislas <stanislas.thibaud@gmail.com>
2023-04-24 14:57:24 +0200

    mousemove js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-const curseur = document.querySelector(".curseur");
-
 window.addEventListener("mousemove", (e) => {
   curseur.style.left = e.pageX + "px";
   curseur.style.top = e.pageY + "px";
+  cercle.style.left = e.pageX + "px";
+  cercle.style.top = e.pageY + "px";
+  rond.style.left = e.pageX + "px";
+  rond.style.top = e.pageY + "px";
 });
